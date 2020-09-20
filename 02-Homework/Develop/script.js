c61c2086f35b523c3a340af56e99948b0170c3e4
Habby Olusesi <habbyolu@gmail.com>
2020-09-20 19:55:59 -0400

    fixed for loop for one condition combination

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -3,55 +3,29 @@ var generateBtn = document.querySelector("#generate");
 var password = document.querySelector("#password")
 var passwordText = "";
 
-var passwordLength = prompt ("How long do you want you password to be. Enter a number no smaller than 8 and no larger than 128.")
-var includeUppercase = confirm ("Would you like to include Uppercase Letters?")
-var includeLowercase = confirm ("Would you like to include Lowercase Letters?")
-var includeNumbers = confirm ("Would you like to include Numbers?")
-var includeSpecialCharacters = confirm ("Would you like to include Special Characters?")
 
 // Write password to the #password input
 function writePassword() {
+
+  var passwordLength = prompt ("How long do you want you password to be. Enter a number no smaller than 8 and no larger than 128.")
+  var includeUppercase = confirm ("Would you like to include Uppercase Letters?")
+  var includeLowercase = confirm ("Would you like to include Lowercase Letters?")
+  var includeNumbers = confirm ("Would you like to include Numbers?")
+  var includeSpecialCharacters = confirm ("Would you like to include Special Characters?")
+
  
-  var allPossibleInputs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~ ";
+  // var allPossibleInputs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~ ";
+  var allPossibleInputs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   
-  if (includeUppercase === true && includeLowercase === false && includeNumbers === false && includeSpecialCharacters === false) {
+  if (includeUppercase === true && includeLowercase === false && includeNumbers === false && includeSpecialCharacters === false) {    
     for (var i = 0; i < passwordLength; i++) {
       passwordText += allPossibleInputs.charAt(Math.floor(Math.random() * allPossibleInputs.length));
     }
-  }
-
+    
   password.textContent = passwordText;
 
+  }
 }
+
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-  // password.appendChild(passwordText)
-  
-
-  // generatePassword(passwordLength);
-
-
-// function generatePassword() {
-
-// var passwordText = "";
-// var allPossibleInputs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~ ";
-// for (var i = 0; i < (passwordLength + 1); i++) {
-//   passwordText += allPossibleInputs.charAt(Math.floor(Math.random() * allPossibleInputs.length));
-// }
-
-// return passwordText;
-
-// var passwordText = document.querySelector("#password");
-// passwordText.value = password;
-// password.appendChild(passwordText)
-
-// }
-
-// Add event listener to generate button
 
